2334d50b4c63adf06ef7efde63b52556e3f17935
faisal-shohag <faisalshohagprog@gmail.com>
2024-07-06 13:27:49 +0600

    updated

--- src/components/app_components/dashboard/UserTable.jsx
+++ src/components/app_components/dashboard/UserTable.jsx
@@ -213,7 +213,7 @@ const UserTable = () => {
 
 <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
         <div className="fixed  inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg rounded-md shadow-2xl space-y-4 border bg-white p-8 dark:bg-gray-900 min-w-[500px]">
+          <DialogPanel className="max-w-lg rounded-md shadow-2xl space-y-4 border bg-white p-8 dark:bg-gray-900 ">
             <DialogTitle className="font-bold">Are you sure?</DialogTitle>
             <Description>This user will be deleted permanantly from the database also its payment history.</Description>
              
